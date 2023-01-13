f31ee81ea1b06530c9b80d93daacf1b51c859173
정혜주 <fixup719@gmail.com>
2023-01-13 17:41:39 +0900

    docs : react 공부

--- ReactJS_02/crypto-tracker/src/routes/Coin.tsx
+++ ReactJS_02/crypto-tracker/src/routes/Coin.tsx
@@ -1,6 +1,15 @@
-import { useEffect, useState } from "react";
-import { Switch, Route, useLocation, useParams } from "react-router";
+// import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import {
+  Switch,
+  Route,
+  useLocation,
+  useParams,
+  useRouteMatch,
+} from "react-router";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoinInfo, fetchCoinTickers } from "../api";
 import Chart from "./Chart";
 import Price from "./Price";
 
@@ -41,6 +50,7 @@ const OverviewItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 33%;
   span:first-child {
     font-size: 10px;
     font-weight: 400;
@@ -52,6 +62,29 @@ const OverviewItem = styled.div`
 // 설명글
 const Description = styled.p`
   margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    padding: 7px 0px;
+    display: block;
+  }
 `;
 
 // 타입을 알려줌
@@ -134,29 +167,49 @@ interface PriceData {
 }
 
 function Coin() {
-  const [loading, setLoading] = useState(true);
-
   // typescript에 url에 어떤 파라미터가 있는지 알려줘야함
   // 1. interface를 쓰거나 2. 직접  {coinId: string}이라고 넣어주거나
   const { coinId } = useParams<RouteParams>();
-
-  // react router dom이 보내주는 location object에 접근
-  const location = useLocation();
-  console.log(location);
 
   // 역시나 typescript에 타입을 알려줘야 함 => 인터페이스 쓰기
   const { state } = useLocation<RouteState>();
   // console.log(state.name);
 
+  const priceMatch = useRouteMatch("/:coinId/price");
+  const chartMatch = useRouteMatch("/:coinId/chart");
+  // 유저가 "url"에 있는 걸 알려줌
+
+  // react router dom이 보내주는 location object에 접근
+  const location = useLocation();
+  console.log(location);
+
+  //모든 query는 고유의 id를 가져야함
+  //React query가 query를 array로 보고 있음
+  // isLoading 이름 지정, data 이름 지정
+  // interface로 타입 알려주기 <>
+  const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>(
+    ["info", coinId],
+    () => fetchCoinInfo(coinId)
+  );
+  const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
+    ["tickers", coinId],
+    () => fetchCoinTickers(coinId)
+  );
+  /* reactquery 사용하면서 생략
+  const [loading, setLoading] = useState(true);
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
 
   useEffect(() => {
     (async () => {
-      const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
       console.log("코인", coinId);
       console.log("info", infoData);
-      const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
       console.log("priceInfo", priceData);
 
       setInfo(infoData);
@@ -164,16 +217,22 @@ function Coin() {
       setLoading(false); //얘를 안해주면 계속 Loading 상태로 남는다.
     })();
   }, [coinId]); // [coinId] => coinId가 변하면 위 useEffect안의 코드가 다시 실행된다.
+*/
 
   // state?.name으로 하는 이유
   // state가 생성되려면 Home화면을 먼저 열어야 하는데,
   // 그렇지 않고 직접 url을 치고 들어가면 state.name이 undefined 되면서 에러가 뜬다.
   // {state?.name || "Loading..."} => state가 존재하면 name을 가져오고 아니면 Loading 띄우기
+
+  // loading은 infoLoading이나 tickersLoading이 될 수 있다.
+  const loading = infoLoading || tickersLoading;
   return (
     <Container>
       <Header>
         {/* loading ? "Loading..." : info?.name => 별도의 URL로 접속한 경우 */}
-        <Title>{state?.name ? state.name : loading ? "Loading..." : info?.name}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+        </Title>
       </Header>
       {loading ? (
         <Loader>Loading...</Loader>
@@ -182,34 +241,45 @@ function Coin() {
           <Overview>
             <OverviewItem>
               <span>Rank:</span>
-              <span>{info?.rank}</span>
+              <span>{infoData?.rank}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Symbol:</span>
-              <span>${info?.symbol}</span>
+              <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Open Source:</span>
-              <span>{info?.open_source ? "Yes" : "No"}</span>
+              <span>{infoData?.open_source ? "Yes" : "No"}</span>
             </OverviewItem>
           </Overview>
-          <Description>{info?.description}</Description>
+          <Description>{infoData?.description}</Description>
           <Overview>
             <OverviewItem>
               <span>Total Suply:</span>
-              <span>{priceInfo?.total_supply}</span>
+              <span>{tickersData?.total_supply}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Max Supply:</span>
-              <span>{priceInfo?.max_supply}</span>
+              <span>{tickersData?.max_supply}</span>
               {/* priceInfo가 존재할 경우에만 max_supply를 찾음 */}
             </OverviewItem>
           </Overview>
+
+          <Tabs>
+            {/* isActive prop 보내기 => 현재 해당 url에 없다면 */}
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+
           <Switch>
-            <Route path={`/${coinId}/price`}>
+            <Route path={`/:coinId/price`}>
               <Price />
             </Route>
-            <Route path={`/${coinId}/chart`}>
+            <Route path={`/:coinId/chart`}>
               <Chart />
             </Route>
           </Switch>
--- ReactJS_02/crypto-tracker/src/routes/Coins.tsx
+++ ReactJS_02/crypto-tracker/src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -82,7 +84,7 @@ const Img = styled.img`
   margin-right: 10px;
 `;
 
-interface CoinInterface {
+interface ICoin {
   id: string;
   name: string;
   symbol: string;
@@ -93,16 +95,20 @@ interface CoinInterface {
 }
 
 function Coins() {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);
+  // reactquery 사용하면서 생략
+  // const [coins, setCoins] = useState<ICoin[]>([]);
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await fetch("https://api.coinpaprika.com/v1/coins");
+  //     const json = await response.json();
+  //     setCoins(json.slice(0, 100));
+  //     setLoading(false);
+  //   })();
+  // }, []);
+  // 아래 추가
+  // dataType 알려주기 => ICoin[]
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
 
   return (
     <Container>
@@ -116,25 +122,32 @@ function Coins() {
           </Coin>
         ))}
       </CoinsList> */}
-      {loading ? (
-        <Loader>Loading...</Loader>
-      ) : (
-        <CoinsList>
-          {coins.map((coin) => (
-            <Coin key={coin.id}>
-              <Link
-                to={{
-                  pathname: `/${coin.id}`,
-                  state: { name: coin.name },
-                }}
-              >
-                <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
-                {coin.name} &rarr;
-              </Link>
-            </Coin>
-          ))}
-        </CoinsList>
-      )}
+      {
+        /*loading*/ isLoading ? (
+          <Loader>Loading...</Loader>
+        ) : (
+          <CoinsList>
+            {
+              // 100개의 코인만 보여주고 싶다면 slice 함수 사용
+              /*coins*/ data?.slice(0, 100).map((coin) => (
+                <Coin key={coin.id}>
+                  <Link
+                    to={{
+                      pathname: `/${coin.id}`,
+                      state: { name: coin.name },
+                    }}
+                  >
+                    <Img
+                      src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                    />
+                    {coin.name} &rarr;
+                  </Link>
+                </Coin>
+              ))
+            }
+          </CoinsList>
+        )
+      }
     </Container>
   );
 }
